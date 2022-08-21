5d2e2bf4ba36ecba5079d083199969d764e2d7d1
alin <alin2wy@163.com>
2022-08-22 04:52:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author alin
+ * @create 2022-08-22 4:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
